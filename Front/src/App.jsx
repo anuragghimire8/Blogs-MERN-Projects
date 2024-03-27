8af4f9c449be6214613ac6170f90678162806048
unknown <tankaghimire3@gmail.com>
2024-03-27 11:43:28 +0545

    navbar-created

---
+++ Front/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Header from './components/Header'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Header/>
+    
+    <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/about" element={<About/>}/>
+    <Route path="/sign-in" element={<Signin/>}/>
+    <Route path="/sign-up" element={<Signup/>}/>
+    <Route path="/dashboard" element={<Dashboard/>}/>
+    <Route path="/projects" element={<Projects/>}/>
+
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
