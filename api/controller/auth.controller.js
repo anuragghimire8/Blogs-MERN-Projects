52cf5e2676a4231afd7e805efceaf55ea030e6ff
unknown <tankaghimire3@gmail.com>
2024-04-02 06:05:18 +0545

    signIn created

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";  
 import { errorHandler } from "../utils/error.js";
+import jwt  from "jsonwebtoken";
 
 export const signup=async(req,res,next)=>{
     const {username,email,password}=req.body;
@@ -26,3 +27,38 @@ export const signup=async(req,res,next)=>{
      }
 }
 
+export const signin=async (req,res,next)=>{
+   const {email,password}=req.body;
+   
+   if(!email || !password || email === "" || password === ""){
+      next(errorHandler(400,"All fields are Required"));
+   }
+   try{
+      const validUser=await User.findOne({email})
+      if(!validUser){
+         next(errorHandler(404,"User Not Found"));
+      }
+      const validPassword=bcryptjs.compareSync(password,validUser.password)
+      if(!validPassword){
+       return  next(errorHandler(400,"Invalid Password"))
+      }
+
+      const token=jwt.sign(
+         {
+            id:validUser._id,
+
+
+         },
+         process.env.JWT_SECRET_KEY,
+
+      )
+      res.status(200).cookie("Access token",token,{
+         httpOnly:true
+      }).json(validUser);
+
+   }catch(err){
+      next(err)
+      console.log("error is ", err)
+   }
+      
+}
