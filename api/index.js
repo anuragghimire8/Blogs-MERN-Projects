535baadb18f65ab7d551ef0cefa839d1a7ce75eb
unknown <tankaghimire3@gmail.com>
2024-04-06 20:29:42 +0545

    pree-installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.route.js"
 import postRoutes from "./routes/post.route.js"
 import cookieParser from "cookie-parser"
 import commentRoutes from './routes/comment.route.js';
+import path from "path"
 
 dotenv.config()
 
@@ -15,10 +16,14 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(error)
 })
 
+ const __dirname=path.resolve()
+
 const app=express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+
 
 
 app.listen(
@@ -31,7 +36,11 @@ app.use("/api/auth",authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
+app.use(express.static(path.join(__dirname,"/client/dist")));
 
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"front","dist","index.html"))
+})
 
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode || 500;
