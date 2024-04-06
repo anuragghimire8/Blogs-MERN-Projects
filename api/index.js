deba6dee0a77a1d4616b87d8415fef017cbec929
unknown <tankaghimire3@gmail.com>
2024-04-06 23:56:51 +0545

    index

--- api/index.js
+++ api/index.js
@@ -38,7 +38,7 @@ app.use('/api/comment', commentRoutes);
 
 app.use(express.static(path.join(__dirname,"/Front/dist")));
 
-app.get("*",(req,res)=>{
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname,"Front","dist","index.html"))
 })
 
